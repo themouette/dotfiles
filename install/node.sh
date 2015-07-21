4ace16f1e4896c25024ca043958381419b7845d3
Julien Muetton <julien@muetton.me>
2015-07-21 15:35:22 +0200

    Do not redownload node version if already there

--- install/node.sh
+++ install/node.sh
@@ -7,15 +7,16 @@ NODE_PATH="${HOME}/nodejs"
 # create directory
 mkdir -p "$NODE_PATH" ; cd "$NODE_PATH"
 
-# get archive
-wget "http://nodejs.org/dist/v${VERSION}/node-v${VERSION}-${PLATFORM}.tar.gz" -O /tmp/node.tar.gz
-
-# extract binaries
-tar xvzf /tmp/node.tar.gz
+if [[ ! -d "${NODE_PATH}/node-v${VERSION}-${PLATFORM}" ]]; then
+    # get archive
+    wget "http://nodejs.org/dist/v${VERSION}/node-v${VERSION}-${PLATFORM}.tar.gz" -O /tmp/node.tar.gz
+    # extract binaries
+    tar xvzf /tmp/node.tar.gz
+fi
 
 # install binaries globally
-sudo ln -s "${NODE_PATH}/node-v${VERSION}-${PLATFORM}/bin/npm" /usr/bin/npm
-sudo ln -s "${NODE_PATH}/node-v${VERSION}-${PLATFORM}/bin/node" /usr/bin/node
+sudo ln -fs "${NODE_PATH}/node-v${VERSION}-${PLATFORM}/bin/npm" /usr/bin/npm
+sudo ln -fs "${NODE_PATH}/node-v${VERSION}-${PLATFORM}/bin/node" /usr/bin/node
 
 # Do not require sudo for npm global backages
 npm config set prefix '${HOME}/.npm-packages'
@@ -23,4 +24,4 @@ npm config set prefix '${HOME}/.npm-packages'
 npm install -g npm@latest
 sudo ln -fs "${HOME}/.npm-packages/bin/npm" /usr/bin/npm
 
-npm install -g eslint
+npm install -g eslint babel-eslint
