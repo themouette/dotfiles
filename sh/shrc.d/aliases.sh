be6b3d35a5be7ed5d0a724be0382333086361022
Julien Muetton <julien@muetton.me>
2019-08-29 18:00:29 +0200

    Add vim-conflict alias

--- sh/shrc.d/aliases.sh
+++ sh/shrc.d/aliases.sh
@@ -4,3 +4,5 @@ alias my_ip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 alias myip6="curl http://ipv6.whatismyip.akamai.com/"
 alias my_ip6="curl http://ipv6.whatismyip.akamai.com/"
+
+alias vim-conflict="vim -p \$(git ls-files -m| uniq)"
